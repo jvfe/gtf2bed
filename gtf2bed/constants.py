d9d9a65308756c6336f42becfeca9e745b91ffc5
jvfe <jvfecav@gmail.com>
2021-03-20 15:17:37 -0300

    feat: Improve feature support with ncRNA

---
+++ gtf2bed/constants.py
@@ -0,0 +1,46 @@
+FEATURES = set(
+    [
+        "five_prime_utr",
+        "three_prime_utr",
+        "CDS",
+        "exon",
+        "intron",
+        "start_codon",
+        "stop_codon",
+        "ncRNA",
+    ]
+)
+
+NON_CODING_BIOTYPES = set(
+    [
+        "Mt_rRNA",
+        "Mt_tRNA",
+        "miRNA",
+        "misc_RNA",
+        "rRNA",
+        "scRNA",
+        "snRNA",
+        "snoRNA",
+        "ribozyme",
+        "sRNA",
+        "scaRNA",
+        "lncRNA",
+        "ncRNA",
+        "Mt_tRNA_pseudogene",
+        "tRNA_pseudogene",
+        "snoRNA_pseudogene",
+        "snRNA_pseudogene",
+        "scRNA_pseudogene",
+        "rRNA_pseudogene",
+        "misc_RNA_pseudogene",
+        "miRNA_pseudogene",
+        "non_coding",
+        "known_ncrna ",
+        "lincRNA ",
+        "macro_lncRNA ",
+        "3prime_overlapping_ncRNA",
+        "vault_RNA",
+        "vaultRNA",
+        "bidirectional_promoter_lncRNA",
+    ]
+)
--- gtf2bed/main.py
+++ gtf2bed/main.py
@@ -1,10 +1,10 @@
 from gtfparse import read_gtf
+from .constants import FEATURES, NON_CODING_BIOTYPES
 import logging
 import click
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    encoding="utf-8",
     level=logging.INFO,
 )
 
@@ -25,7 +25,8 @@ logging.basicConfig(
 @click.option(
     "--feature",
     required=True,
-    help="The name of the feature of the GTF you want to extract.",
+    help="A comma-separated list of feature names to extract.",
+    type=click.STRING,
 )
 @click.option(
     "--compressed",
@@ -36,12 +37,38 @@ logging.basicConfig(
 def gtf2bed(gtf, output, feature, compressed):
     """Simple command to convert GTF features to a BED file"""
 
+    # TODO: Support the following features:
+    ## On feature column (Done):
+    # 5’ UTR
+    # 3’ UTR
+    # CDS
+    # Exons
+    # Introns
+    # Start codons
+    # Stop codons
+    ## Computed:
+    # Non-coding RNA (Done)
+    # First exons
+    # Last Exons
+
+    feature_list = set(feature.split(","))
+
     logging.info(f"Reading {gtf}...")
     current_gtf = read_gtf(gtf, chunksize=1024)
 
-    features = set([feature])
-    logging.info(f"Filtering {gtf} for {'; '.join(features)}...")
-    feature_gtf = current_gtf[current_gtf["feature"].isin(features)]
+    logging.info(f"Filtering {gtf} for {'; '.join(feature_list)}...")
+    feature_gtf = current_gtf[current_gtf["feature"].isin(feature_list)]
+
+    if "ncRNA" in feature_list:
+        feature_gtf = feature_gtf[
+            feature_gtf["transcript_biotype"].isin(NON_CODING_BIOTYPES)
+        ]
+
+    if feature_gtf.empty:
+        nl = "\n"
+        message = f"File is empty, check that the feature provided is contained below:\n{nl.join(FEATURES)}"
+        logging.error(message)
+        raise Exception
 
     bed_data = feature_gtf[
         ["seqname", "start", "end", "gene_id", "score", "strand"]
